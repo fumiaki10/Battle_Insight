5ca8a923fb82778ffa78a9934c607e46182dfcde
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-05-12 13:42:31 +0900

    ダメージとk/dを取れるように実装
    ・切り抜いてグレースケールを入れて精度を上げる

--- ocr_png_to_txt.py
+++ ocr_png_to_txt.py
@@ -13,6 +13,11 @@ image = Image.open("test.png")
 #左、上、右、下　この位置から内側を切り抜き
 cropped = image.crop((0, 200, 1200, 900))
 
+#ダメージ列を取る
+damage_area = image.crop((650,250,950,920))
+
+damage_area.save("damage_test.png")
+
 #グレースケール化
 gray = cropped.convert("L")
 
@@ -24,8 +29,25 @@ bw = big.point(lambda x: 0 if x < 160 else 255)
 
 bw.save("processed.png")
 
-
 #imageをstring型、日本語にしてocrにする
-text = pytesseract.image_to_string(image, lang="jpn")
+text = pytesseract.image_to_string(
+  damage_area,
+  lang="jpn",
+  config="--psm 6 -c tessedit_char_whitelist=0123456789"
+)
 
-print(text)
+#k/dを取りに行く
+kd_area = image.crop((950,250,1150,920))
+kd_gray = kd_area.convert("L")
+kd_big = kd_gray.resize((kd_gray.width * 3, kd_gray.height * 3))
+kd_bw = kd_big.point(lambda x: 0 if x < 180 else 255)
+
+kd_bw.save("kd_test.png")
+
+kd_text = pytesseract.image_to_string(
+  kd_bw,
+  lang="eng",
+  config="--psm 6 -c tessedit_char_whitelist=0123456789/"
+)
+
+print(kd_text)
